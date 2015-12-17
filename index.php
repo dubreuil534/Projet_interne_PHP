974da16557fd52a72fcd1904e08d8077484182c2
Dominic Dubreuil <dubreuil534@hotmail.com>
2015-12-17 10:46:09 -0500

    Ajouter index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    //initialisation ici
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>$Title$</title>
+</head>
+<body>
+<?php
+    echo 'test';
+ ?>
+</body>
+</html>
