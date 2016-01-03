51fbf7447586e9f6c24264f2d7a1abfe108843b4
Dominic Dubreuil <dubreuil534@hotmail.com>
2016-01-03 15:55:44 -0500

    Ajouter la connexion du tableau avec le fichier dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -2,3 +2,12 @@
 require_once "view_part/_page_base.php";
 $site_data[PAGE_ID] = "dashboard";
 require_once "db/_talkmsg_data.php";
+?>
+<ul>
+    <li class="tmsg_cont" style="background-color:"<?php echo $talk_msg_data[0] ?>>
+    <div class="tmsg_head">
+        <span class="tmsg_time"><?php echo $talk_msg_data[1] </span>
+        <span class="tmsg_username"><?php echo $talk_msg_data[1]</span>
+</div>
+<p class>
+
