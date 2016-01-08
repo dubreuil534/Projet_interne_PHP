d8c7c26bc44e193e52f4168b2a610f401b6f3a4c
Dominic Dubreuil <dubreuil534@hotmail.com>
2016-01-07 19:38:55 -0500

    Travailler sur le fichier loginout_form

--- view_part/_loginout_form.php
+++ view_part/_loginout_form.php
@@ -1,5 +1,25 @@
-<?php var_dump($site_data); if(! $site_data[USER_IS_LOGGED]){ ?>
-    <form id="login" name="login">
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = " ";
+if (isset($_POST['login'])) {
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+// connection a la base de donnee mysql
+    $conn = new mysqli($servername, $username, $password);
+    echo '<p>' . "on est connecter" . '</p>';
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+}
+
+ if( !$site_data[USER_IS_LOGGED]){
+?>
+    <form id="login" name="login" method="post">
         <label for="username">Pseudo :</label>
         <input type="text" name="username" id="username" />
         <label for="password">Mot de passe :</label>
@@ -8,6 +28,6 @@
     </form>
 <?php } else { ?>
     <form id="logout" name="logout">
-        <input type="submit" name="dologout" value="Sortir"/>
+        <input type="submit" name="login" value="Sortir"/>
     </form>
 <?php } ?>
